13b02adc088014e5cc3939944a0f11f7d5cef5ac
YoMyBoy <77848492+YoMyBoy@users.noreply.github.com>
2022-09-07 22:48:41 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -42,7 +42,7 @@ Config.announcementLength = 6000;
  * Image Filename
  * DROP IMAGE IN "images" FOLDER
  */
-Config.backgroundImage = "";
+Config.backgroundImage = "default.jpg";
 
 /**
  * Enable debug messages?
